b8c14f7a81a9a3148c7adc97e148b9d6d065517e
Amrit Singh <asingh.analytics@gmail.com>
2020-02-12 13:53:09 -0500

    change BarPlot to function declaration

--- src/components/BarPlot/BarPlot.js
+++ src/components/BarPlot/BarPlot.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 import { PrepareData } from './prepareData.js'
 
-const BarPlot = props => {
+function BarPlot(props){
   const { x, y, xlab, ylab, title, ...rest } = props;
 
   const plot = PrepareData(x, y, xlab, ylab, title)
